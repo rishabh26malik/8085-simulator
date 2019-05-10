189752eba2f39dd99065e0b16cf3480dc46ff432
rishabh26malik <32366430+rishabh26malik@users.noreply.github.com>
2019-05-10 16:37:04 +0530

    Add files via upload

---
+++ arithmetic.h
@@ -0,0 +1,16 @@
+#ifndef _ARITHMETICN_H
+#define _ARITHMETIC_H
+
+void inr(string [], int);
+void dcr(string [], int);
+void inx(string [], int);
+void dcx(string [], int);
+void add(string [], int);
+void adi(string [], int);
+void sub(string [], int);
+void sui(string [], int);
+void dad(string [], int);
+void cmp(string [], int);
+void cma(string [], int);
+
+#endif
