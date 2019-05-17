e2e6137027b3ddfcbab82f0cb8b72956a64163c3
rishabh26malik <32366430+rishabh26malik@users.noreply.github.com>
2019-05-17 19:08:56 +0530

    Add files via upload

---
+++ BREAK.cpp
@@ -0,0 +1,12 @@
+#include "library.h"
+#include "debug.h"
+#include "tools.h"
+
+extern vector<string> address;
+
+void break()
+{
+    int lineNo;
+    cin>>lineNo;
+
+}
---
+++ commands.h
@@ -0,0 +1,22 @@
+#ifndef _COMMANDS_H
+#define _COMMANDS_H
+
+void mvi(string [], int );
+void mov(string [], int );
+void lda(string [], int);
+void lda(string [], int);
+void sta(string [], int);
+void lhld(string [], int);
+void shld(string [], int);
+void lxi(string [], int);
+void stax(string [], int);
+void xchg(string [], int);
+void store(string [], int);
+void jmp(string [], int);
+void jc(string [], int);
+void jnc(string [], int);
+void jz(string [], int);
+void jnz(string [], int);
+
+#endif
+
