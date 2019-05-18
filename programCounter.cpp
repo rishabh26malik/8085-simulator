31eb8046674374d4d62ec4a804703482e3f0033b
rishabh26malik <32366430+rishabh26malik@users.noreply.github.com>
2019-05-18 13:04:56 +0530

    Add files via upload

---
+++ programCounter.cpp
@@ -0,0 +1,69 @@
+#include "library.h"
+#include "tools.h"
+
+extern string pc;
+
+string programCounter(string cmd)
+{
+    int adder;
+	if(cmd=="mvi")
+		adder=3;
+	else if(cmd=="mov")
+		adder=1;
+    else if(cmd=="inr")
+		adder=1;
+    else if(cmd=="dcr")
+		adder=1;
+    else if(cmd=="inx")
+		adder=1;
+    else if(cmd=="dcx")
+		adder=1;
+    else if(cmd=="lhld")
+		adder=3;
+    else if(cmd=="shld")
+		adder=3;
+    else if(cmd=="sta")
+		adder=3;
+    else if(cmd=="lda")
+		adder=3;
+    else if(cmd=="add")
+		adder=2;
+    else if(cmd=="adi")
+		adder=2;
+    else if(cmd=="sub")
+		adder=2;
+    else if(cmd=="sui")
+		adder=2;
+    else if(cmd=="jmp")
+		adder=3;
+    else if(cmd=="cmp")
+		adder=1;
+    else if(cmd=="lxi")
+		adder=3;
+    else if(cmd=="stax")
+		adder=3;
+    else if(cmd=="dad")
+		adder=1;
+    else if(cmd=="store")
+		adder=4;
+    else if(cmd=="cmp")
+		adder=1;
+    else if(cmd=="jnz")
+		adder=3;
+    else if(cmd=="jz")
+		adder=3;
+    else if(cmd=="jnc")
+		adder=3;
+    else if(cmd=="jc")
+		adder=3;
+	else if(cmd=="xchg")
+		adder=1;
+    else if(cmd=="cma")
+		adder=1;
+
+    int decPC=hexToDec(pc);
+    decPC+=adder;
+    pc=decToHex(decPC);
+    reverse(pc.begin(),pc.end());
+    return pc;
+}
---
+++ runProgram.cpp
@@ -0,0 +1,123 @@
+#include "library.h"
+#include "commands.h"
+#include "arithmetic.h"
+#include "jump.h"
+
+extern int pc;
+
+void runprog(string cmd[],int len )
+{
+	if(cmd[0]=="mvi")
+	{
+		mvi(cmd,len);
+	}
+	else if(cmd[0]=="mov")
+	{
+		mov(cmd,len);
+	}
+
+	else if(cmd[0]=="sta")
+	{
+		sta(cmd,len);
+	}
+	else if(cmd[0]=="lda")
+	{
+		lda(cmd,len);
+	}
+	else if(cmd[0]=="inr")
+	{
+		inr(cmd,len);
+	}
+	else if(cmd[0]=="dcr")
+	{
+		dcr(cmd,len);
+	}
+	else if(cmd[0]=="lhld")
+	{
+		lhld(cmd,len);
+	}
+	else if(cmd[0]=="shld")
+	{
+		shld(cmd,len);
+	}
+    else if(cmd[0]=="lxi")
+	{
+		lxi(cmd,len);
+	}
+	else if(cmd[0]=="stax")
+	{
+		stax(cmd,len);
+	}
+	else if(cmd[0]=="xchg")
+	{
+		xchg(cmd,len);
+	}
+	else if(cmd[0]=="inx")
+	{
+		inx(cmd,len);
+	}
+	else if(cmd[0]=="dcx")
+	{
+		dcx(cmd,len);
+	}
+	else if(cmd[0]=="add")
+	{
+		add(cmd,len);
+	}
+	else if(cmd[0]=="adi")
+	{
+		adi(cmd,len);
+	}
+	else if(cmd[0]=="sub")
+	{
+		sub(cmd,len);
+	}
+	else if(cmd[0]=="sui")
+	{
+		sui(cmd,len);
+	}
+    else if(cmd[0]=="jmp")
+	{
+		jmp(cmd,len);
+	}
+	else if(cmd[0]=="jnz")
+	{
+		jnz(cmd,len);
+	}
+	else if(cmd[0]=="jz")
+	{
+		jz(cmd,len);
+	}
+	else if(cmd[0]=="jnc")
+	{
+		jnc(cmd,len);
+	}
+	else if(cmd[0]=="jc")
+	{
+		jc(cmd,len);
+	}
+	else if(cmd[0]=="cmp")
+	{
+		cmp(cmd,len);
+	}
+	else if(cmd[0]=="dad")
+	{
+		dad(cmd,len);
+	}
+	else if(cmd[0]=="store")
+	{
+		store(cmd,len);
+	}
+	else if(cmd[0]=="cma")
+	{
+		cma(cmd,len);
+	}
+	else if(cmd[0]=="hlt")
+	{
+
+	}
+	else
+    {
+        cout<<"error "<<pc<<" : invalid command"<<endl;
+    }
+}
