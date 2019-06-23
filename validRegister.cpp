f87b6da5c8b14cee1f912c2cfcdb0aa8bef7598a
rishabh26malik <32366430+rishabh26malik@users.noreply.github.com>
2019-06-23 15:17:27 +0530

    Delete validRegister.cpp

--- validRegister.cpp
+++
@@ -1,9 +0,0 @@
-#include "library.h"
-int validRegister(string reg)
-{
-	if(reg=="a" || reg=="b" || reg=="c" || reg=="d" || reg=="e" || reg=="h" || reg=="l" || reg=="m")
-		return 1;
-	else
-		return 0;
-
-}
